cab64c16379264602fa08c5ede565f57dfd3cd4a
huyld <huyld@users.noreply.github.com>
2019-04-15 17:28:46 +0700

    feat(account): add `list-account` API ✨️

---
+++ functions/account/list.js
@@ -0,0 +1,44 @@
+import * as dynamoDbLib from 'libs/dynamodb-lib';
+import { success, failure } from 'libs/response-lib';
+
+/**
+ * Get list accounts
+ *
+ * @export
+ * @param {*} event
+ * @param {*} context
+ * @param {function} callback
+ * @returns
+ */
+export async function main(event, context, callback) {
+  console.log('List accounts queryStringParameters', event.queryStringParameters);
+
+  // Filter accounts by request params
+  let conditionClause = '';
+  let conditionExpression = {};
+  if (event.queryStringParameters && event.queryStringParameters['currency']) {
+    conditionClause += 'AND begins_with(accountId, :currency)';
+    conditionExpression = {
+      ...conditionExpression,
+      ':currency': event.queryStringParameters['currency']
+    }
+  }
+
+  const params = {
+    TableName: 'accounts',
+    KeyConditionExpression: `userId = :userId ${conditionClause}`,
+    ExpressionAttributeValues: {
+      ':userId': event.requestContext.identity.cognitoIdentityId,
+      ...conditionExpression
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call('query', params);
+    // Return the matching list of items in response body
+    return success(result.Items);
+  } catch (e) {
+    console.error('List accounts:', e);
+    return failure({ status: false });
+  }
+}
